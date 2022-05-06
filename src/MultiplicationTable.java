5f15dd10c4ccc550be0aa6c42ac7d7dbf0b397f5
krishnanair165 <krishnanair165@gmail.com>
2022-05-06 06:28:22 +0100

    first commit

---
+++ src/MultiplicationTable.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+        int number,multiplier;
+        multiplier=1;
+        Scanner intput = new Scanner(System.in);
+        System.out.println("Enter the number");
+        number=intput.nextInt();
+        while (multiplier <=10){
+
+            System.out.printf("%d * %d = %d\n", number,multiplier,(number * multiplier));
+            multiplier++;
+        }
+
+
+
+
+    }
+
+
+}
